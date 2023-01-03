41d66d5b25bf8f81e09712bdf5e1bc308b8d66d6
gh <12345678@qq.com>
2023-01-03 22:47:20 +0800

    封装轻提示

--- js/common.js
+++ js/common.js
@@ -5,3 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+// 封装轻提示
+const toastBox = document.querySelector('#myToast')
+const toast = new bootstrap.Toast(toastBox,{
+animation:true,//是否有动画
+autohide:true,//是否自动隐藏
+delay:3000//停留多久
+})
+
+const tip = (msg) =>{
+toastBox.querySelector('.toast-body').innerHTML = msg
+}
