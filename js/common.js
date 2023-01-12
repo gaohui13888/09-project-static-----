9c4c9fdc9c3230a00b83876437f427791a421bb2
gh <12345678@qq.com>
2023-01-12 09:27:38 +0800

    注册功能完成

--- js/common.js
+++ js/common.js
@@ -6,6 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 200)
 })
 
+// ------------------------------------------------
+// axios的公共配置
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
+
+// axios 的其他配置
+
+
+// ------------------------------------------------
+
 // 封装轻提示
 const toastBox = document.querySelector('#myToast')
 const toast = new bootstrap.Toast(toastBox,{
@@ -13,6 +22,7 @@ animation:true,//是否有动画
 autohide:true,//是否自动隐藏
 delay:3000//停留多久
 })
+
 
 const tip = (msg) =>{
 toastBox.querySelector('.toast-body').innerHTML = msg
